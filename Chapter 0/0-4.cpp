2752e31f68159998891086ae5d89e18f7b86efd1
Justin Ferrer <luxuryjow@gmail.com>
2024-01-25 22:32:39 -0400

    initial commit

---
+++ Chapter 0/0-4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() 
+{
+    std::cout << "//a small C++ program\n" << std::endl;
+    std::cout << "#include <iostream>\n" << std::endl;
+    std::cout << "int main() \n{" << std::endl;
+    std::cout <<"\tstd::cout << \"Hello, world!\" << std::endl; " << std::endl;
+    std::cout << "\treturn 0;\n" << std::endl;
+    std::cout << "}" << std::endl;
+    return 0;
+}
